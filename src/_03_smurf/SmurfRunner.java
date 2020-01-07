4939828c6873de6c4d709bc303bb6edaf33e57cf
League Student <league@iMac-9.attlocal.net>
2020-01-06 19:55:44 -0800

    eeee

--- src/_03_smurf/SmurfRunner.java
+++ src/_03_smurf/SmurfRunner.java
@@ -3,22 +3,24 @@ package _03_smurf;
 public class SmurfRunner {
 public static void main(String[] args) {
 
-	
-	
+	Smurf handy = new Smurf("handy");
+handy.getName();	
+handy.eat();
 
 
 
+Smurf pappa = new Smurf("pappa");
+
+System.out.println(pappa.getHatColor());
+
+System.out.println(pappa.isGirlOrBoy());
 
 
 
+Smurf smurfette = new Smurf("smurfette");
 
-
-
-
-
-
-
-
+System.out.println( smurfette.getHatColor());
+System.out.println( smurfette.isGirlOrBoy());
 
 
 
